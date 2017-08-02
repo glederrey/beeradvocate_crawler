ae0aa6787bbe77da561812aea1b63600a9f845bb
glederrey <gael.lederrey@epfl.ch>
2017-08-02 14:24:13 +0200

    [UPD] Error in parser

--- code/classes/parser.py
+++ code/classes/parser.py
@@ -613,7 +613,7 @@ class Parser:
                             date = int(datetime.datetime(year, month, day, 12, 0).timestamp())
 
                             # Check if there's some text
-                            if 'characters' in g.group(4) and not '0 characters':
+                            if 'characters' in g.group(4) and '0 characters' not in g.group(4):
                                 str_2 = '(.+?)<br>(.+?)<span class="muted">(.+?) characters</span><br><br><div>'
                                 grp2 = re.search(str_2, g.group(4))
 
